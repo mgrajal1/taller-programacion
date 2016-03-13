d4f0a8f729fe963bc90cbf2902272d41da142a20
Mateo Grajales Jaramillo <mgrajal1@hotmail.com>
2016-03-12 22:05:14 -0500

    start

---
+++ taller programacion/taller programacion/taller programacion.cpp
@@ -0,0 +1,18 @@
+// taller programacion.cpp : Defines the entry point for the console application.
+//
+/*
+El almacén Electro S.A requiere un sistema para capturar y facturar las ventas realizadas de sus 3 productos, Televisor, 
+equipo de sonido y IPod. Se deberá capturar el voltaje y el precio de cada producto, el tamaño del televisor, la cantidad 
+de cd’s del equipo y la capacidad en Gigabytes del IPod. La factura generada debe tener un consecutivo, el nombre del 
+producto, el porcentaje de descuento el cual será ingresado en el momento de generar la factura y el total a pagar. La 
+factura se podrá imprimir tantas veces el usuario lo requiera
+*/
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
